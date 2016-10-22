b6313fc36c5c09042f75e137f8502ff1c716ab55
Natalia Adamus <nataliadamus94@gmail.com>
2016-10-22 02:07:20 +0200

    First commit

---
+++ Hanger/Controllers/LoginController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hanger.Models;
+namespace Hanger.Controllers
+{
+    public class LoginController : Controller
+    {
+        private HangerEntities db = new HangerEntities();
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            Console.WriteLine("Login");
+            System.Diagnostics.Debug.Write("Login");
+            return View();
+
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User u)
+        {
+
+            ModelState.Remove("ConfirmPassword");
+            ModelState.Remove("Photo");
+            ModelState.Remove("Description");
+            ModelState.Remove("Mail");
+            ModelState.Remove("Date_access");
+
+            // this action is for handle post (login)
+            if (ModelState.IsValid) // this is check validity
+            {
+               
+                {
+                    Console.WriteLine("Zalogowano");
+                    var v = db.User.Where(a => a.Profil_name.Equals(u.Profil_name) && a.Password.Equals(u.Password)).FirstOrDefault();
+
+                    if (v != null)
+                    {
+                        Session["LogedUserID"] = v.Id.ToString();
+                        Session["LogedUserFullname"] = v.Profil_name.ToString();
+                        if (v.Profil_name.ToString() == "admin")
+                        {
+                            return RedirectToAction("AfterLoginAdmin");
+                        }
+                        return RedirectToAction("AfterLogin");
+                    }
+                }
+            }
+            return View(u);
+        }
+        public ActionResult AfterLogin()
+        {
+            if (Session["LogedUserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ Hanger/Controllers/UserProfilController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hanger.Models;
+
+namespace Hanger.Controllers
+{
+    public class UserProfilController : Controller
+    {
+        // GET: UserProfil
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult SubmitPhoto()
+        {
+            HttpPostedFileBase file = Request.Files[0];
+            byte[] imageSize = new byte[file.ContentLength];
+            file.InputStream.Read(imageSize, 0, (int)file.ContentLength);
+
+
+            using (HangerEntities DataContext = new HangerEntities())
+            {
+                Photos p = new Photos();
+                p.AdId = 1;
+                p.Name = file.FileName;
+
+                if (DataContext.Photos != null && DataContext.Photos.Count() != 0)
+                {
+                    p.Id = (from ph in DataContext.Photos
+                                 select ph.Id).Max() + 1;
+                }
+                else
+                    p.Id = 0;
+
+               
+                p.Type = file.ContentType;
+                DataContext.Photos.Add(p);
+                DataContext.SaveChanges();
+            }
+
+            return RedirectToAction("UserWall", "Wall");
+        }
+    }
+}
